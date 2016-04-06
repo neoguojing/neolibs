b629177588d5374aa6cd5a866d8f22211c8d4502
neoguojing <guojing_neo@163.com>
2016-04-06 18:58:20 +0800

    add timer

---
+++ src/libs/neotimer.cpp
@@ -0,0 +1,71 @@
+﻿
+#include "neoindex.h"
+#include "neotimer.h"
+
+namespace NEOLIB{
+
+#ifdef WIN32
+NeoTimer::NeoTimer():
+    m_hTimerQueue(INVALID_HANDLE_VALUE)
+{
+    m_hTimerQueue = CreateTimerQueue();
+    if (NULL == m_hTimerQueue)
+    {
+        printf("CreateTimerQueue failed (%d)\n", GetLastError());
+        return;
+    }
+}
+#else
+NeoTimer::NeoTimer():
+{
+
+}
+#endif
+
+NeoTimer::~NeoTimer()
+{
+#ifdef WIN32
+    if (!DeleteTimerQueue(m_hTimerQueue))
+        printf("DeleteTimerQueue failed (%d)\n", GetLastError());
+#else
+
+#endif   
+}
+
+bool NeoTimer::CreateTimer(string timername, void* param)
+{
+#ifdef WIN32
+    HANDLE tTimer = INVALID_HANDLE_VALUE;
+    if (!CreateTimerQueueTimer(&tTimer, m_hTimerQueue, (WAITORTIMERCALLBACK)TimerRoutine, param,0, 1000, WT_EXECUTEDEFAULT))
+        return false;
+    m_hTimers.insert(pair<string,HANDLE>(timername, tTimer));
+#else
+
+#endif   
+    return true;
+}
+
+bool NeoTimer::DeleteTimer(string timername)
+{
+#ifdef WIN32
+    map<string, HANDLE>::iterator iter; 
+    iter = m_hTimers.find(timername);
+    if (INVALID_HANDLE_VALUE==iter->second)
+    {
+        m_hTimers.erase(iter);
+        return false;
+    }
+    DeleteTimerQueueTimer(m_hTimerQueue,iter->second,NULL);
+    m_hTimers.erase(iter);
+#else
+
+#endif   
+    return true;
+}
+
+void NeoTimer::TimerRoutine(void* lpParam, bool TimerOrWaitFired)
+{
+
+}
+
+};//NEOLIB
---
+++ src/libs/neotimer.h
@@ -0,0 +1,39 @@
+﻿#ifndef NEOTIMER
+
+#define NEOTIMER
+
+#define NEOMAXTIMERCOUNT 1024
+#include<string>
+#include<map>
+using namespace std;
+
+namespace NEOLIB {
+
+
+class NeoTimer{
+
+public:
+    NeoTimer();
+    ~NeoTimer();
+
+    bool CreateTimer(string timername, void* param=NULL);
+    bool DeleteTimer(string timername);
+
+    static void TimerRoutine(void* lpParam, bool TimerOrWaitFired);
+
+private:
+
+#ifdef WIN32
+    map<string,HANDLE> m_hTimers;
+    HANDLE m_hTimerQueue;
+#else
+
+#endif
+
+};
+
+
+
+}//NEOLIB
+
+#endif
